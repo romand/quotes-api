e6505bd8e6db519232c3e3a8b82d3dfc15df5516
Roman Dzvinkovsky <romand.ne@gmail.com>
2021-06-14 13:28:53 +0200

    Add test for POST /quotes.

--- src/App.hs
+++ src/App.hs
@@ -1,24 +1,28 @@
-module App
-  ( main
-  ) where
+module App where
 
 import Data.Aeson (Value(String, Number), (.=), object)
 import Data.Text.Lazy (toStrict)
 import qualified Network.HTTP.Types as HTTP
 import Web.Scotty
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
 
 import Quote
 import DB
 
+
+app :: IO Application
+app = scottyApp $ do
+  post "/quotes" $ do
+    (quote :: Quote) <- jsonData
+    (QuoteID id) <- liftAndCatchIO $ createQuote quote
+    status HTTP.created201
+    json $ object [("status", "created"), ("id", Number (fromIntegral id))]
+
+  notFound $ json $ object [("error", "not found")]
+
+  defaultHandler $ \e ->
+    json $ object [("error", String (toStrict e))]
+
 main :: IO ()
-main = scotty 3000 $ do
-    post "/quotes" $ do
-      (quote :: Quote) <- jsonData
-      (QuoteID id) <- liftAndCatchIO $ createQuote quote
-      status HTTP.created201
-      json $ object [("status", "created"), ("id", Number (fromIntegral id))]
-
-    notFound $ json $ object [("error", "not found")]
-
-    defaultHandler $ \e ->
-      json $ object [("error", String (toStrict e))]
+main = app >>= run 3000
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,61 @@
+{-# LANGUAGE QuasiQuotes #-}
+import Test.Hspec
+import Test.Hspec.Wai
+import Test.Hspec.Wai.JSON
+import GHC.Generics
+import Data.Text.Lazy (Text)
+import Data.Aeson
+import Data.Aeson.QQ
+import Data.ByteString.Lazy.UTF8 (toString)
+
+
+import App (app)
+
+quoteJson :: Value
+quoteJson =
+  [aesonQQ|{
+    "applicantName": "name",
+    "insuranceStartDate": "2013-10-17T09:42:49.007Z",
+    "insuranceEndDate": "2013-10-17T09:42:49.007Z",
+    "insuredItemPrice": 12.345
+  }|]
+
+data CreateResp =
+  CreateResp { status :: Text
+             , quoteId :: Int
+             } deriving (Generic, Show)
+quoteNoPrefix "quoteId" = "id"
+quoteNoPrefix s = s
+instance FromJSON CreateResp where
+  parseJSON = genericParseJSON $ defaultOptions
+    { fieldLabelModifier = quoteNoPrefix }
+
+spec :: Spec
+spec = with app $ do
+
+  describe "GET /asdf123" $ do
+    it "responds with 404" $ do
+      get "/asdf123" `shouldRespondWith` 404
+
+  describe "POST /quotes" $ do
+    let q = encode quoteJson
+
+    it "responds with 201" $ do
+      post "/quotes" q `shouldRespondWith` 201
+
+    it "responds with json" $ do
+      post "/quotes" q `shouldRespondWith` 201 {
+        matchHeaders=["Content-Type" <:> "application/json; charset=utf-8"]
+      }
+
+    let matcher _ body =
+          case decode body of
+            Just(CreateResp{status = "created" , quoteId = _}) -> Nothing
+            Nothing -> Just $ "unexpected response: " ++ toString body
+
+    it "responds with quote id" $ do
+      post "/quotes" q `shouldRespondWith` 201 {matchBody=MatchBody matcher}
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
